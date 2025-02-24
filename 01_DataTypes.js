b3c39e6e06c4068024096233d670d5638a3aad7f
MirBytes <mirmutabassimali@gmail.com>
2025-02-24 13:16:04 +0500

    DatatypesDone

--- 01_DataTypes.js
+++ 01_DataTypes.js
@@ -1,3 +1,30 @@
 console.log("Hello");
 
+let name = "Mir"
+let age = 18
+let isloggedin = true
+
+// Premetive
+// number 2 => power of 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => null
+// undefined => undefined
+// symbols => unique
+
+// Non-Premetive
+//object
+
+
+console.log(name.length);
+// console.log(age.length); // error
+console.log(isloggedin.toString());
+// console.log(isloggedin.length); // error
+console.log(isloggedin.valueOf());
+// console.log(isloggedin.value); // error
+
+console.log(typeof undefined);
+console.log(typeof null);
+
 
